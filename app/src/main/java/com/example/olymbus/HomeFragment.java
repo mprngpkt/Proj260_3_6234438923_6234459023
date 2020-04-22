5a46c21edc4a141317c86cc0e27d5fba7c58ade6
mprngpkt <mp.mppkt@gmail.com>
2020-04-23 00:33:24 +0700

    start onclick list

--- app/src/main/java/com/example/olymbus/HomeFragment.java
+++ app/src/main/java/com/example/olymbus/HomeFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
@@ -14,6 +19,24 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        String [] eventMenu = {"Swimming 1","Swimming 2","Swimming 3"};
+
+        ListView listView = view.findViewById(R.id.eventListView);
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>( getActivity() ,
+                android.R.layout.simple_list_item_1 , eventMenu);
+        listView.setAdapter(listViewAdapter);
+
+/*        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+            }
+        });*/
+
+        return view;
     }
 }
