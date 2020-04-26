528a2b6dcaefbe61d308b3191f22b788b0f71311
mprngpkt <mp.mppkt@gmail.com>
2020-04-26 16:37:08 +0700

    cannot cast android.app.Fragment to com.example.olymbus.DetailFragment

--- app/src/main/java/com/example/olymbus/MainActivity.java
+++ app/src/main/java/com/example/olymbus/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.olymbus;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,8 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
-    }
 
+        if(findViewById(R.id.displayList)!=null){
+            ListFragment listFragment = new ListFragment();
+            listFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.displayList,
+                    listFragment).commit();
+        }
+
+        if(findViewById(R.id.displayDetail)!=null){
+            getFragmentManager().popBackStack();
+            DetailFragment detailFragment = new DetailFragment();
+            detailFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.displayDetail,
+                    detailFragment).commit();
+        }
+
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,4 +61,10 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             };
+
+
+/*    public void onMobileSelected(String Model, int position){
+        DetailFragment detailFragment = (DetailFragment)
+                getFragmentManager().findFragmentById(R.id.displayDetail);
+    }*/
 }
