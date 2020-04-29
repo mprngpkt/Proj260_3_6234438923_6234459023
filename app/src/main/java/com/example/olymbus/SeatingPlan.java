a99067478d1db00e36b5ac8ecfb30f14b2821b99
thispan <panda131243@gmail.com>
2020-04-29 21:38:01 +0700

    gridview in seating plan

--- app/src/main/java/com/example/olymbus/SeatingPlan.java
+++ app/src/main/java/com/example/olymbus/SeatingPlan.java
@@ -6,15 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class SeatingPlan extends Fragment {
-    Button btn;
+    Button btnBack;
+    Button btnseat1A;
 
 
     //เชื่อมกับ .xml เมื่อใช้ extends fragment
@@ -22,9 +22,13 @@ public class SeatingPlan extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.layout_seatingplan, container, false);
 
-        btn = (Button) v.findViewById(R.id.btnBack);
+        //1A
 
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        //back
+        btnBack = (Button) v.findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), BusTimeTable.class);
