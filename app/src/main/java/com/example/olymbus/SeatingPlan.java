d97df43d06580ccbd1d3e458161081f2da9ebc2d
thispan <panda131243@gmail.com>
2020-05-01 08:57:14 +0700

    add book alertDialog

--- app/src/main/java/com/example/olymbus/SeatingPlan.java
+++ app/src/main/java/com/example/olymbus/SeatingPlan.java
@@ -1,6 +1,11 @@
 package com.example.olymbus;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +15,17 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 public class SeatingPlan extends Fragment {
     Button btnBack;
+    Button btnBook;
     Button btnseat1A;
 
 
     //เชื่อมกับ .xml เมื่อใช้ extends fragment
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.layout_seatingplan, container, false);
@@ -35,6 +43,25 @@ public class SeatingPlan extends Fragment {
                 startActivity(intent);
             }
         });
+
+        //book with alert dialog
+        btnBook = (Button) v.findViewById(R.id.btnBook);
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog =new AlertDialog.Builder(getContext());
+                alertDialog.setMessage("Hey");
+                alertDialog.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"Good",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.show();
+
+            }
+        });
+
 
         return v;
     }
