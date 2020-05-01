098a257c89cd7a2262f5e982ef2c02d8d0f83033
mprngpkt <mp.mppkt@gmail.com>
2020-05-01 11:50:55 +0700

    ceremony button activate

--- app/src/main/java/com/example/olymbus/Ceremony.java
+++ app/src/main/java/com/example/olymbus/Ceremony.java
@@ -1,14 +1,18 @@
 package com.example.olymbus;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Ceremony extends AppCompatActivity {
+public class Ceremony extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ceremony);
+    public View onCreateView(@NonNull final LayoutInflater inflater,
+                             @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ceremony, container, false);
     }
 }
--- app/src/main/java/com/example/olymbus/HomeFragment.java
+++ app/src/main/java/com/example/olymbus/HomeFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +21,7 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
-    private Button startButton;
+    private ImageButton ceremonyBut;
     private TextView textView;
 
     @Nullable
@@ -29,25 +30,24 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        startButton = (Button) view.findViewById(R.id.homeButton);
+        ceremonyBut = (ImageButton) view.findViewById(R.id.ceremonyButton);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        ceremonyBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent(getActivity(), EventListActivity.class);
-                startActivity(in);
+                Intent intent = new Intent(getActivity(), Ceremony.class);
+                startActivity(intent);
             }
-
+        });
         /*String[] eventItems = {"Ceremony Opening Ceremony", "Athletics Marathon Final",
                 "Swimming Preliminary", "Swimming Freestyle/Medley", "Diving Springboard Semifinal",
                 "Diving Synchronized 3m Springboard Final", "Diving 10m Platform Semifinal",
                 "Weightlifting 40 kg Group B", "Weightlifting 76 kg Group B", "Weightlifting 109 kg Group B",
-                "Weightlifting 109 kg Group A & Victory Ceremony"};*/
-        //final ListView listView = view.findViewById(R.id.eventListView);
+                "Weightlifting 109 kg Group A & Victory Ceremony"};
 
-        });
+        final ListView listView = view.findViewById(R.id.eventListView);
 
-/*        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(),
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1,eventItems);
         listView.setAdapter(listViewAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -55,14 +55,12 @@ public class HomeFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
-                    case 0:
+                    case 0: Intent in = new Intent(getActivity(), Ceremony.class);
+                            startActivity(in);
                     case 1: textView.setText("Click 2");
-                    case 3: textView.setText("Click 3");
+                    case 2: textView.setText("Click 3");
 
-                }
-
-            }
-        });*/
+                }*/
         return view;
     };
 
